fa28d7c8d25edbf619b9f6bdcd9bc60b5ce13aa9
marcos <marcos.alvarez@tolber.io>
2021-12-10 18:56:56 -0300

    add image in document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,6 +36,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta property="og:image" content="/images/PortfolioV2.jpg"></meta>
         </Head>
         <body>
           <Main />
